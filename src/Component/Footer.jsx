5745aa6520f950c8569f027726d7b50b1a7af956
habibullayev7558883@gmail.com <habibullayev7558883@gmail.com>
2026-05-14 11:16:13 +0500

    I added footer

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -14,9 +14,12 @@ const Footer = () => {
         <div className="container">
             <div className="footer__wrapper">
                 <ul className='footer__list'>
-                    <li className="footer__item">
+
+                    <li className="footer__item space">
                         <a href="#"><img src={logo} alt="logo" /></a>
-                        <p>Revolutioning the way people and company works</p>
+                        <div className="footer__box1">
+                        <p className='footer__desc'>Revolutioning the way people and company works</p>
+                        </div>
                         <div className="footer__link-img">
                             <a target='__blank' href="https://www.facebook.com/?locale=ru_RU"><img src={facebook} alt="facebook" /></a>
                             <a target='__blank' href="https://www.instagram.com/"><img src={instagram} alt="instagram" /></a>
@@ -25,10 +28,33 @@ const Footer = () => {
                             <a target='__blank' href="https://uz.linkedin.com/"><img src={linkedin} alt="linkedin" /></a>
                         </div>
                     </li>
-                    <li className="footer__item"></li>
-                    <li className="footer__item"></li>
-                    <li className="footer__item"></li>
+
+                    <li className="footer__item links">
+                        <a className='footer__link-title' href="quick links">Quick Links</a>
+                        <a className='footer__link' href="home">Home</a>
+                        <a className='footer__link' href="facilities">Facilities</a>
+                        <a className='footer__link' href="spacess">Spaces</a>
+                        <a className='footer__link' href="community">Community</a>
+                        <a className='footer__link' href="contact">Contact</a>
+                    </li>
+
+                    <li className="footer__item community">
+                        <a className='footer__link-title' href="community">Community</a>
+                        <a className='footer__link' href="events">Events</a>
+                        <a className='footer__link' href="upcoming">Upcoming</a>
+                        <a className='footer__link' href="articles">Articles</a>
+                        <a className='footer__link' href="magazine">Magazine</a>
+                    </li>
+
+                    <li className="footer__item">
+                        <a className='footer__link-title' href="newsletter">Subscribe to our newsletter</a>
+                        <a className='footer__address' href="Email address">Email address</a>
+                    </li>
                 </ul>
+
+                <div className="footer__bottom">
+                    <p className='footer__bottom-text'>©2021 qSpace. All rights reserved <hr /></p>
+                </div>
             </div>
         </div>
     </footer>
